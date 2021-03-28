57e49483cfb4b13b6a6153898a511edaf261cd6c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-28 08:29:00 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -7,8 +7,7 @@ import requests
 
 import os
 from helpers.filters import command, other_filters
-from helpers.decorators import errors
-
+from helpers.wrappers import errors
 
 @Client.on_message(command("song") & other_filters)
 @errors
@@ -17,8 +16,8 @@ async def a(client, message: Message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = await message.reply("🔎 Searching the song...")
-    ydl_opts = {"format": "bestaudio"}
+    m = await message.reply('ðŸ”Ž Searching the song...')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = []
         count = 0
@@ -46,29 +45,29 @@ async def a(client, message: Message):
             open(thumb_name, 'wb').write(thumb.content)
 
         except Exception as e:
-            m.edit(f"Found nothing. Try changing the spelling a little.\n\n`{e}`")
+            m.edit(f"Found nothing. Try changing the spelling a little.\n\n{e}")
             return
     except Exception as e:
         m.edit(
-            "✖️ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
+            "âœ–ï¸ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
         )
         print(str(e))
         return
-    m.edit("⏬ Downloading.")
+    await m.edit("â¬ Downloading.")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎧 **Title**: [{title[:35]}]({link})\n⏳ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`'
+        rep = f'ðŸŽ§ Title: [{title[:35]}]({link})\nâ³ Duration: {duration}\nðŸ‘â€ðŸ—¨ Views: {views}'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
-        m.delete()
+        await  message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
+        await m.delete()
     except Exception as e:
-        m.edit(f"❌ Error!! \n\n`{e}`")
+        m.edit(f"âŒ Error!! \n\n{e}")
     try:
         os.remove(audio_file)
         os.remove(thumb_name)
