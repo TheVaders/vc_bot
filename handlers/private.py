abbfc33f79c92080d5d0be2885948f830d94037c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-04-29 07:26:06 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -5,7 +5,7 @@ from config import BOT_NAME as bn
 from helpers.filters import other_filters2
 
 
-@Client.on_message(other_filters2)
+@Client.on_message(command("start") & other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
         f"""I am **{bn}** !!
