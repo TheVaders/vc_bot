cae6cbba6009e3c655b15ba394cd972228fbae6d
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-08 13:00:14 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -4,13 +4,13 @@ import youtube_dl
 from youtube_search import YoutubeSearch
 import requests
 
-from helpers.filters import command, other_filters2
+from helpers.filters import command, other_filters2, other_filters
 from helpers.decorators import errors
 
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Voice
 
-from config import BOT_NAME as bn
+from config import BOT_NAME as bn, PLAY_PIC
 
 
 @Client.on_message(command("start") & other_filters2)
@@ -38,19 +38,13 @@ Enjoy Streaming Music 😉
     )
 
 
-@Client.on_message(command("help") & other_filters2)
-async def helper(ok, message: Message):
-    await message.reply_text(
-        f"""💞 Hello! Following are the commands available for **{bn}** - __A Group Voice Chat Music Player__.
-
-The commands I currently support are:
-⚜️ /play - **[ Groups Only ]** > __Plays the replied audio file or YouTube video through link.__
-⚜️ /song - **[ Groups & DM ]** > __Uploads the searched song in the chat.__
-⚜️ /ytplay - **[ Groups Only ]** > __Plays the song directly from YouTube Search.__
-⚜️ /pause - **[Groups Only ]** > __Pause Voice Chat Music.__
-⚜️ /resume - **[Groups Only ]** > __Resume Voice Chat Music.__
-⚜️ /skip - **[Groups Only ]** > __Skips the current Music Playing In Voice Chat.__
-⚜️ /stop - **[Groups Only ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
+@Client.on_message(command("ping") & other_filters)
+async def ping(_, message: Message):
+    hell_pic = PLAY_PIC
+    await message.reply_photo(
+    photo=hell_pic,
+    caption="I'm Alive and working fine. Do /help to get commands.\n\nHappy Streaming Music 😉",
+)
 
 
 @Client.on_message(command("song") & other_filters2)
