f16ee499cd45254f7113119b9666fe075924c3a9
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-08 13:34:32 +0530

    Update helper.py

--- handlers/helper.py
+++ handlers/helper.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import BOT_NAME as bn
-from helpers.filters import other_filters2, other_filters
+from helpers.filters import command, other_filters2, other_filters
 
 
 
