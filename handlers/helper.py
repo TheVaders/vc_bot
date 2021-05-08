7fae420ce89bb6b1e34a10673eb775d03fd03a53
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-05-08 13:06:23 +0530

    Create helper.py

---
+++ handlers/helper.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2, other_filters
+
+
+
+@Client.on_message(command("help") & other_filters2)
+async def helper(ok, message: Message):
+    await message.reply_text(
+        f"""💞 Hello! Following are the commands available for **{bn}** - __A Group Voice Chat Music Player__.
+The commands I currently support are:
+
+🔥 **Users Commands :**
+⚜️ /play - **[ Groups Only ]** > __Plays the replied audio file or YouTube video through link.__
+⚜️ /song - **[ Groups & DM ]** > __Uploads the searched song in the chat.__
+⚜️ /ytplay - **[ Groups Only ]** > __Plays the song directly from YouTube Search.__
+
+🔰 **Admin & Sudo Users Commands :**
+⚜️ /pause - **[Groups Only ]** > __Pause Voice Chat Music.__
+⚜️ /resume - **[Groups Only ]** > __Resume Voice Chat Music.__
+⚜️ /skip - **[Groups Only ]** > __Skips the current Music Playing In Voice Chat.__
+⚜️ /stop - **[Groups Only ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
+
+@Client.on_message(command("help") & other_filters)
+async def ghelp(_, message: Message):
+    await message.reply_text(f"**{bn} :-** Hey! PM me to get all the commands 😉")
