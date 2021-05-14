baade7e796b9aa0613f5ab489a0ea382c9252d67
Hokkabazvevo <82279865+Hokkabazvevo@users.noreply.github.com>
2021-05-14 23:25:31 +0300

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -17,8 +17,9 @@ async def a(client, message: Message):
     query = ''
     for i in message.command[1:]:
         query += ' ' + str(i)
-    print(query)
-    m = await message.reply(f"**{Bn} :-** {query} Adlı parça aranıyor..")
+    okvai = query.capitalize()
+    print(query.capitalize())
+    m = await message.reply(f"**{Bn} :-** 🔍 Searching for {okvai}")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = []
@@ -47,11 +48,11 @@ async def a(client, message: Message):
             open(thumb_name, 'wb').write(thumb.content)
 
         except Exception as e:
-            m.edit(f"**{Bn} :-** Üzgünüm ne aradığını anlayamadım🥺 yazıyı biraz değiştirerek tekrar dene.\n\n{e}")
+            m.edit(f"**{Bn} :-** 😕 Found nothing. Try changing the spelling a little.\n\n{e}")
             return
     except Exception as e:
         m.edit(
-           f"**{Bn} :-** Üzgünüm ne aradığını bulamadım🥺\n\nFarklı bir şekilde yazarak tekrar dene."
+           f"**{Bn} :-** 😕 Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
         )
         print(str(e))
         return
@@ -61,7 +62,7 @@ async def a(client, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 **İsim:** [{title[:35]}]({link})\n⏳ **Süre:** {duration}\n👀 **İzlenme:** {views}'
+        rep = f'🎶 **Title:** [{title[:35]}]({link})\n⏳ **Duration:** {duration}\n👀 **Views:** {views}'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
