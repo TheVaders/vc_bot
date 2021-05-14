b186f3e0f5ecee39367a0c4df03a5536af9b2d8e
Hokkabazvevo <82279865+Hokkabazvevo@users.noreply.github.com>
2021-05-15 01:16:34 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,14 +8,17 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""Selam ben **{bn}** !!
+        f"""Selam Ben `{bn}`
+
 Sesli sohbetlerde müzik dinlemenize olanak sağlarım.
+
 Kullanma kılavuzu:
 💠 /play - __Parçayı oynatmaya yarayan komut.__
 💠 /pause - __Botu durdurmaya yarayan komut.__
 💠 /resume - __Botu devam ettirmeye yarayan komut.__
 💠 /skip - __Diğer şarkıya geçmeye yarayan komut.__
-💠 /stop - __Botu kapatır.__
+💠 /stop - __Botu kapatmaya yarayan komut.__
+
 🤖 **Developer by @Zep_Unb**
         """,
         reply_markup=InlineKeyboardMarkup(
