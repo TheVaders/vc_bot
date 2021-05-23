3469c269c2678e83a4705ed9faf943b339ac210c
Hokkabazvevo <82279865+Hokkabazvevo@users.noreply.github.com>
2021-05-23 21:08:07 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -41,4 +41,4 @@ async def play(_, message: Message):
         await message.reply_text(f"Müziği başarıyla #{await callsmusic.queues.put(message.chat.id, file_path=file_path)} sıraya ekledim")
     else:
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
-        await message.reply_text(f"Oynatılıyor...")
+        await message.reply_text(f"`Oynatılıyor...`")
